2cca52567d5b157585173568c67cbcb08c2400f1
41ucet <41ucet2020@gmail.com>
2025-03-09 02:46:31 +0530

    Fix

--- src/pages/About/Career/Components/Partner.jsx
+++ src/pages/About/Career/Components/Partner.jsx
@@ -1,6 +1,7 @@
 import { FaRegLightbulb, FaMedal, FaSearch, FaCheckCircle } from "react-icons/fa";
 import { AiOutlineRocket, AiOutlineCheckCircle } from "react-icons/ai";
 import { FaLightbulb } from "react-icons/fa6";
+import { NumberTicker } from "@/components/magicui/number-ticker";
 
 export default function Partner() {
 
