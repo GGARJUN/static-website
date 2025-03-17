9e2fb822225dbb8574150ff756436925ea2cb479
41ucet <41ucet2020@gmail.com>
2025-03-17 14:52:35 +0530

    fix the folder

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -31,14 +31,14 @@ const pages = [
 const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${pages
-        .map(
-            (page) => `
+      .map(
+        (page) => `
     <url>
         <loc>${baseUrl}${page}</loc>
         <priority>0.8</priority>
     </url>`
-        )
-        .join("")}
+      )
+      .join("")}
 </urlset>`;
 
 const sitemapPath = path.join(__dirname, "../public/sitemap.xml");
