462b2b7be4abd5bc7f71eab007a5dbfb1f076086
41ucet <41ucet2020@gmail.com>
2025-03-17 14:42:03 +0530

    fix sitemap

---
+++ public/sitemap.xml/scripts/generate-sitemap.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const path = require("path");
+
+const baseUrl = "https://electricalstaticwebsite.netlify.app"; // Change this to your domain
+
+const pages = [
+    "/",
+    "/about/who_we_are",
+    "/about/careers",
+    "/about/partners",
+    "/engineering_design_services/hardware",
+    "/engineering_design_services/firmware",
+    "/engineering_design_services/software",
+    "/engineering_design_services/mechanical",
+    "/electronic_design/pcb_design_service",
+    "/electronic_design/pcb_layout",
+    "/electronic_design/analysis",
+    "/electronic_design/reverse_engineering",
+    "/electronic_design/design_modification_service",
+    "/electronic_design/design_for_manufacturability",
+    "/manufacturing/pcba",
+    "/manufacturing/test_fixture",
+    "/manufacturing/box_build",
+    "/iot",
+    "/resources/blogs",
+    "/resources/news",
+    "/case-study",
+    "/contact-us",
+];
+
+const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${pages
+        .map(
+            (page) => `
+    <url>
+        <loc>${baseUrl}${page}</loc>
+        <priority>0.8</priority>
+    </url>`
+        )
+        .join("")}
+</urlset>`;
+
+const sitemapPath = path.join(__dirname, "../public/sitemap.xml");
+
+fs.writeFileSync(sitemapPath, sitemapContent);
+
+console.log("✅ Sitemap generated at: /public/sitemap.xml");
